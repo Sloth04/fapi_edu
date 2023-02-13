dfa3dcfed89300c03b9f6b3601dcb4ac36694cb4
Oleksandr Chepurnyi <hepurnyi_oo@groupbwt.com>
2023-02-13 13:51:11 +0200

    feat(*) add middlewares

--- app/main.py
+++ app/main.py
@@ -2,12 +2,16 @@ import pathlib
 import uvicorn
 import app.models as models
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqladmin import Admin
 from app.internal.admin import UserAdmin
 from app.routers import writers_router, books_router, users_router
 from database import engine
+from middleware import TimerMiddleware
 from settings import logger
+
+origins = ["http://localshost:8080"]
 
 cwd = pathlib.Path().cwd()
 models.Base.metadata.create_all(bind=engine)
@@ -20,6 +24,9 @@ admin.add_view(UserAdmin)
 application.include_router(users_router)
 application.include_router(books_router)
 application.include_router(writers_router)
+
+application.add_middleware(TimerMiddleware)
+application.add_middleware(CORSMiddleware, allow_origins=origins)
 
 
 @application.get("/")
